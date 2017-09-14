7e7e7df140c1c384a14855ed2950a0c64aaf27cf
Drew <andrewslawson42@gmail.com>
2017-09-14 13:48:15 -0400

    Added styling and data

--- src/App.js
+++ src/App.js
@@ -19,6 +19,60 @@ var app = new Vue({
         image: '/img/bisexual.svg',
         definition: 'definition of bisexual'
       }
+    ],
+    romanticItems: [{
+        name: 'Heteroromantic',
+        image: '/img/heteroromantic.svg',
+        definition: 'definition of heteroromantic'
+      },
+      {
+        name: 'Homoromantic',
+        image: '/img/homoromantic.svg',
+        definition: 'definition of homoromantic'
+      },
+      {
+        name: 'Biromantic',
+        image: '/img/biromantic.svg',
+        definition: 'definition of biromantic'
+      }
+    ],
+    genderItems: [{
+        name: 'Cisgender',
+        image: '/img/cisgender.svg',
+        definition: 'definition of cisgender'
+      },
+      {
+        name: 'Transgender',
+        image: '/img/transgender.svg',
+        definition: 'definition of transgender'
+      }
+    ],
+    pronounItems: [{
+        name: 'He/Him',
+        image: '/img/hehim.svg',
+        definition: 'definition of he/him'
+      },
+      {
+        name: 'She/Her',
+        image: '/img/sheher.svg',
+        definition: 'definition of she/her'
+      },
+      {
+        name: 'They/Them',
+        image: '/img/theythem.svg',
+        definition: 'definition of they/them'
+      }
+    ],
+    interestItems: [{
+        name: 'Activist',
+        image: '/img/activist.svg',
+        definition: 'definition of activist'
+      },
+      {
+        name: 'Postgender',
+        image: '/img/postgender.svg',
+        definition: 'definition of postgender'
+      }
     ]
   }
 });
